b505f19668d60cf058760240438316272d8215b4
DimensionStudios <piggyandbobo@icloud.com>
2025-07-25 18:49:06 -0700

    added Evanslib file

---
+++ evanlib.cs
@@ -0,0 +1,85 @@
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace evanslib{
+
+    class Evanslib{
+
+
+        public static void Print<T>(T value){
+            if (value is Dictionary<string, object> dict){
+                foreach (var kvp in dict){
+                    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+                }        
+            }
+            
+            else{
+                Console.WriteLine(value);
+            }
+}
+
+        public static string Input(){
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)){
+                
+                exitError("Input of 'null'");
+                return "";
+            }
+            return input;
+        }
+
+        public static void currentDate(){
+            DateTime date = DateTime.Now.Date;
+            Print(date.ToString("MM/dd/yyyy"));
+        }
+
+        public static void currentTimeMT(){
+            DateTime time = DateTime.Now;
+            Print(time.ToString("HH:mm:ss"));
+        }
+
+        public static void currentTimeUT(){
+            DateTime time = DateTime.Now;
+            Print(time.ToString("hh:mm:ss tt"));
+        }
+
+        public static void currentDT(){
+            DateTime dt = DateTime.Now;
+            Print(dt);
+        }
+
+        public static void currentMonth(){
+            DateTime dt = DateTime.Now;
+
+            Print(dt.ToString("MMM"));
+        }
+
+        public static Dictionary<string, object> Dict(){
+            var evanslibdict = new Dictionary<string, object>();
+                Print(evanslibdict);
+                return evanslibdict;
+        }
+
+        public static void ModifyDict(Dictionary<string, object> dict, string name, string value){
+            dict[name] = value;
+        }
+
+        public static void exitError(string crashreason){
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Evanslib ERROR: " + crashreason);
+            Console.ForegroundColor = ConsoleColor.White;
+            Environment.Exit(0);
+        }
+
+        public static void Exit(){
+            Environment.Exit(0);
+        }
+
+
+    }
+
+}
