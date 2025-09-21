d802774e578b2349f83281ec893e1c6982c1dd36
= <piggyandbobo@icloud.com>
2025-09-20 17:09:33 -0700

    fixed issue with other systems running the library

--- examples/login_sys/obj/Debug/net9.0/login_sys.AssemblyInfo.cs
+++ examples/login_sys/obj/Debug/net9.0/login_sys.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("login_sys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+851d76048223a6ed413581cb4153e19d2c2787d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e2e314d1bc5cc16548963159d998a18a8907f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("login_sys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("login_sys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
