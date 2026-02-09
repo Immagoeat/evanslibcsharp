70df03584046aeb8f9c8263ba918b0d25896ba85
Immagoeat <95148457+Immagoeat@users.noreply.github.com>
2026-02-09 16:21:53 +0000

    Trying to update to 1.3.8

--- examples/login_sys/obj/Debug/net9.0/login_sys.AssemblyInfo.cs
+++ examples/login_sys/obj/Debug/net9.0/login_sys.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("login_sys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5701afd76bbc6d7ecbcb40e042a1a1f84020e76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09743a6a6ff733f1bfc922f7274a1fba42a0abd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("login_sys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("login_sys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- examples/minimalist_calendar/obj/Debug/net9.0/minimalist_calendar.AssemblyInfo.cs
+++ examples/minimalist_calendar/obj/Debug/net9.0/minimalist_calendar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimalist_calendar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5701afd76bbc6d7ecbcb40e042a1a1f84020e76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09743a6a6ff733f1bfc922f7274a1fba42a0abd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimalist_calendar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimalist_calendar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
